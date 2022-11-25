5804d1fb1a0b2bf3e8ae5ae8f8bb333687028b5b
Michael Della Bitta <mdellabitta@tutanota.com>
2022-11-25 16:42:53 -0500

    added c subnet, subnet groups

--- network.tf
+++ network.tf
@@ -12,3 +12,26 @@ resource "aws_default_subnet" "subnet_a" {
 resource "aws_default_subnet" "subnet_b" {
   availability_zone = data.aws_availability_zones.available.names[1]
 }
+
+resource "aws_default_subnet" "subnet_c" {
+  availability_zone = data.aws_availability_zones.available.names[2]
+}
+
+resource "aws_db_subnet_group" "default" {
+  name        = "main_subnet_group"
+  description = "Main group of subnets"
+  subnet_ids = [
+    aws_default_subnet.subnet_a.id,
+    aws_default_subnet.subnet_b.id,
+    aws_default_subnet.subnet_c.id
+  ]
+}
+
+resource "aws_elasticache_subnet_group" "default" {
+  name       = "main-subnet-group"
+  subnet_ids = [
+    aws_default_subnet.subnet_a.id,
+    aws_default_subnet.subnet_b.id,
+    aws_default_subnet.subnet_c.id
+  ]
+}
