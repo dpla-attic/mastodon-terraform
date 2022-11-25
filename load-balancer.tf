b6a90214b63a35a6a6918411fe29d857dbef97d0
Michael Della Bitta <mdellabitta@tutanota.com>
2022-11-25 16:45:32 -0500

    load balancer

---
+++ load-balancer.tf
@@ -0,0 +1,79 @@
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_default_vpc.vpc.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "alb_sg_rule_ingress_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_sg.id
+  description       = "Allow all inbound traffic on port 80"
+}
+
+resource "aws_security_group_rule" "alb_sg_rule_ingress_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_sg.id
+  description       = "Allow all inbound traffic on port 443"
+}
+
+resource "aws_lb" "alb" {
+  name               = "mastodon-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_default_subnet.subnet_a.id, aws_default_subnet.subnet_b.id]
+
+  enable_deletion_protection = true
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.mastodon_cert.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "404"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
