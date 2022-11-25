6056ea9b8878072f6d96c2af12349721794c0ce1
Michael Della Bitta <mdellabitta@tutanota.com>
2022-11-25 16:43:39 -0500

    working version

--- redis.tf
+++ redis.tf
@@ -1,9 +1,33 @@
-resource "aws_elasticache_cluster" "mastodon_redis" {
-  cluster_id           = "mastodon-redis"
-  engine               = "redis"
-  node_type            = "cache.m4.large"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
-  port                 = 6379
+resource "aws_elasticache_replication_group" "mastodon-redis-cluster" {
+  engine                     = "redis"
+  engine_version             = "7.0"
+  replication_group_id       = "mastodon-redis-cluster"
+  description                = "Mastodon Redis"
+  node_type                  = "cache.m6g.large"
+  port                       = 6379
+  parameter_group_name       = "default.redis7.cluster.on"
+  automatic_failover_enabled = true
+  auto_minor_version_upgrade = true
+
+  num_node_groups         = 2
+  replicas_per_node_group = 1
+
+  maintenance_window       = "sun:00:00-sun:02:00"
+  security_group_ids       = [aws_security_group.mastodon_redis_sg.id]
+  snapshot_retention_limit = 7
+  snapshot_window          = "02:00-04:00"
+  subnet_group_name        = aws_elasticache_subnet_group.default.name
+
+  log_delivery_configuration {
+    destination      = aws_cloudwatch_log_group.mastodon_log_group.id
+    destination_type = "cloudwatch-logs"
+    log_format       = "text"
+    log_type         = "slow-log"
+  }
+}
+
+resource "aws_security_group" "mastodon_redis_sg" {
+  name        = "mastodon-redis-sg"
+  description = "Mastodon Redis"
+  vpc_id      = aws_default_vpc.vpc.id
 }
