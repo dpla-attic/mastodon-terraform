70bb7d6e08d92632f92322dca94859e7e9053476
Michael Della Bitta <mdellabitta@tutanota.com>
2022-12-04 13:16:39 -0500

    cleanup

--- fargate.tf
+++ fargate.tf
@@ -29,11 +29,6 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
 }
 
-# resource "aws_iam_role" "ecs_tasks_execution_role" {
-#   name               = "ecs-tasks-execution-role"
-#   assume_role_policy = data.aws_iam_policy_document.ecs_tasks_execution_role.json
-# }
-
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
